d7ddd586b0c659f443aafe151c55d39d075146dd
Omodamola Oladeji <darmhoo@hotmail.com>
2022-11-08 13:25:58 +0000

    started

---
+++ src/views/StaffLogin.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+interface LoginDetails {
+  staffId: string;
+  password: string;
+}
+
+const loginDetails: LoginDetails = reactive({ staffId: "", password: "" });
+</script>
+
+<template>
+  <form>
+    <div class="">Login Page</div>
+  </form>
+</template>
