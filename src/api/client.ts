69e3845ef4a740a62989491e08b71bfce1b09eb4
Omodamola Oladeji <darmhoo@hotmail.com>
2022-12-01 10:44:20 +0000

    fixed cors

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,32 +1,27 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
-import type { AnyAaaaRecord } from "dns";
+const auth = useAuthStore();
+
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Access-Control-Allow-Credentials': true,
+    Authorization: `Bearer ${auth.token}`,
+  }
 
 });
 
-const auth = useAuthStore();
 export const get = (url: string) =>
   instance({
     method: "GET",
     url,
-    headers: {
-      Authorization: `Bearer ${auth.token}`, 'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
+
   });
 export const post = (url: string, data: object) => {
   return instance({
     method: "POST",
     url,
     data,
-    headers: {
-      Authorization: `Bearer ${auth.token}`, 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-
-    },
   });
 };
 
